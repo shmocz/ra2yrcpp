e504385935d11933cf10560fbf851e94e8b0a179
shmocz <112764837+shmocz@users.noreply.github.com>
2022-02-12 05:51:27 +0200

    add types.h

---
+++ src/types.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdint.h>
+
+#define X(s) typedef uint ## s ## _t u ## s 
+X(8); X(16); X(32); X(64);
+#undef X
+#define X(s) typedef int ## s ## _t i ## s 
+X(8); X(16); X(32); X(64);
+#undef X
+
+typedef u32 addr_t;
+
+union X86Regs {
+  u32 regs[8];
+  struct {
+    u32 eax, ebx, ecx, edx, esi, edi, ebp, esp;
+  };
+};

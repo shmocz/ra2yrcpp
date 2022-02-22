aac55ace8da1ecba04783d6dfca7632f5653752d
shmocz <112764837+shmocz@users.noreply.github.com>
2022-02-23 00:53:40 +0200

    add missing config.hpp

---
+++ src/config.hpp
@@ -0,0 +1,9 @@
+#pragma once
+namespace cfg {
+constexpr unsigned int ACCEPT_TIMEOUT_MS = 250;
+constexpr unsigned SOCKET_SR_TIMEOUT = 5000;
+constexpr unsigned int DEFAULT_BUFLEN = 512;
+constexpr unsigned int MAX_CLIENTS = 16;
+constexpr unsigned int SERVER_PORT = 14521;
+constexpr unsigned int MAX_MESSAGE_LENGTH = 1e7;
+};  // namespace cfg

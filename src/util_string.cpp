a30b7cbaea6cf29d997e2f725c1cfd9b76e7d795
shmocz <112764837+shmocz@users.noreply.github.com>
2022-04-19 05:44:03 +0300

    add split_string utility

---
+++ src/util_string.cpp
@@ -0,0 +1,13 @@
+#include "util_string.hpp"
+#include <regex>
+
+std::vector<std::string> split_string(const std::string& str,
+                                      const std::string delim) {
+  std::vector<std::string> tokens;
+  std::regex re(delim);
+  std::sregex_token_iterator first{str.begin(), str.end(), re, -1}, last;
+  for (; first != last; ++first) {
+    tokens.push_back(*first);
+  }
+  return tokens;
+}
--- src/util_string.hpp
+++ src/util_string.hpp
@@ -1,10 +1,15 @@
 #pragma once
 #include "types.h"
 #include <string>
+#include <vector>
 
 namespace yrclient {
 inline vecu8 to_bytes(std::string msg) { return vecu8(msg.begin(), msg.end()); }
 inline std::string to_string(const vecu8& bytes) {
   return std::string(bytes.begin(), bytes.end());
 }
+
+// split string by delimiter regex and return vector of strings
+std::vector<std::string> split_string(const std::string& s,
+                                      const std::string delim = "[\\s]+");
 }  // namespace yrclient

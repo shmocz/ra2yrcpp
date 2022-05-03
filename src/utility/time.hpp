cabb603f6442bacfc2ec94f2ea4b159845d41c2d
shmocz <112764837+shmocz@users.noreply.github.com>
2022-05-03 04:27:58 +0300

    sleep_ms time util

--- src/utility/time.hpp
+++ src/utility/time.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include <chrono>
+#include <thread>
 
 namespace util {
 inline std::chrono::system_clock::time_point current_time() {
   return std::chrono::system_clock::now();
+}
+
+template <typename T>
+inline void sleep_ms(const T s) {
+    std::this_thread::sleep_for(s);
 }
 }  // namespace util

6d4d0f3f1bdf5fc30550cf6b13d22e8355f3502a
shmocz <112764837+shmocz@users.noreply.github.com>
2022-05-03 03:15:49 +0300

    updates to string utilities
    implement join_string and string_to_hex functions, and fix missing
    namespace for split_string

--- src/util_string.cpp
+++ src/util_string.cpp
@@ -1,8 +1,8 @@
 #include "util_string.hpp"
 #include <regex>
 
-std::vector<std::string> split_string(const std::string& str,
-                                      const std::string delim) {
+std::vector<std::string> yrclient::split_string(const std::string& str,
+                                                const std::string delim) {
   std::vector<std::string> tokens;
   std::regex re(delim);
   std::sregex_token_iterator first{str.begin(), str.end(), re, -1}, last;
@@ -10,4 +10,14 @@ std::vector<std::string> split_string(const std::string& str,
     tokens.push_back(*first);
   }
   return tokens;
+}
+
+std::string yrclient::join_string(const std::vector<std::string> v,
+                                  const std::string delim) {
+  std::stringstream ss;
+  for (size_t i = 1; i < v.size(); i++) {
+    ss << v[i - 1] << delim;
+  }
+  ss << v.back();
+  return ss.str();
 }

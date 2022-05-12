15ca7a5e00cca54ab938dfa94bab158cecb9d292
shmocz <112764837+shmocz@users.noreply.github.com>
2022-05-12 04:53:53 +0300

    pass by value in to_string utility

--- src/util_string.hpp
+++ src/util_string.hpp
@@ -6,7 +6,8 @@
 
 namespace yrclient {
 inline vecu8 to_bytes(std::string msg) { return vecu8(msg.begin(), msg.end()); }
-inline std::string to_string(const vecu8& bytes) {
+
+inline std::string to_string(const vecu8 bytes) {
   return std::string(bytes.begin(), bytes.end());
 }
 

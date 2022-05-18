4b250985a6863fe2efc850c5cac429bd5d3915c1
shmocz <112764837+shmocz@users.noreply.github.com>
2022-05-18 10:22:55 +0300

    update time util

--- src/utility/time.hpp
+++ src/utility/time.hpp
@@ -9,6 +9,6 @@ inline std::chrono::system_clock::time_point current_time() {
 
 template <typename T>
 inline void sleep_ms(const T s) {
-    std::this_thread::sleep_for(s);
+    std::this_thread::sleep_for(std::chrono::milliseconds(s));
 }
 }  // namespace util

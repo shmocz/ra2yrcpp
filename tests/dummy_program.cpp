e8e8d930203dfe1d0fdc373be0528697b2beb790
shmocz <112764837+shmocz@users.noreply.github.com>
2022-05-18 10:19:02 +0300

    add dummy program

---
+++ tests/dummy_program.cpp
@@ -0,0 +1,17 @@
+#include "utility/time.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0] << " count delay" << std::endl;
+    return 1;
+  }
+
+  auto count = std::stoul(argv[1]);
+  auto delay = std::stoul(argv[2]);
+  for (unsigned long i = 0; i < count; i++) {
+    std::cout << i << "/" << count << std::endl;
+    util::sleep_ms(delay);
+  }
+  return 0;
+}

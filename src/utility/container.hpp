266c14c71cee6e7401003f52c1c7bbae505c6a89
shmocz <112764837+shmocz@users.noreply.github.com>
2022-05-18 10:17:50 +0300

    implement vmap utility function

---
+++ src/utility/container.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include "debug_helpers.h"
+
+namespace utility {
+
+///
+/// Apply function op to each element of container U and store results into a
+/// vector.
+///
+template <typename U, typename O>
+inline auto vmap(U input, O op) {
+  using ResT =
+      std::vector<typename std::result_of<O(typename U::value_type)>::type>;
+  if (input.size() == 0) {
+    return ResT{};
+  }
+  ResT res(input.size());
+  std::transform(input.begin(), input.end(), res.begin(), op);
+  return res;
+}
+};  // namespace utility

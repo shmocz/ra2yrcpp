6e7708eead09b07ef3f239069271962a6e3cfa71
shmocz <112764837+shmocz@users.noreply.github.com>
2022-05-26 05:16:43 +0300

    increase default socket read buffer size

--- src/config.hpp
+++ src/config.hpp
@@ -2,7 +2,7 @@
 namespace cfg {
 constexpr unsigned int ACCEPT_TIMEOUT_MS = 250;
 constexpr unsigned SOCKET_SR_TIMEOUT = 5000;
-constexpr unsigned int DEFAULT_BUFLEN = 512;
+constexpr unsigned int DEFAULT_BUFLEN = 4096;
 constexpr unsigned int MAX_CLIENTS = 16;
 constexpr unsigned int SERVER_PORT = 14521;
 constexpr unsigned int MAX_MESSAGE_LENGTH = 1e7;

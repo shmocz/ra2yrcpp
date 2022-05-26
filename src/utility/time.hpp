bab48dc42dd2ac44ea122a764e0f79223b818048
shmocz <112764837+shmocz@users.noreply.github.com>
2022-05-26 05:19:50 +0300

    add call_until helper

--- src/utility/time.hpp
+++ src/utility/time.hpp
@@ -9,6 +9,17 @@ inline std::chrono::system_clock::time_point current_time() {
 
 template <typename T>
 inline void sleep_ms(const T s) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(s));
+  std::this_thread::sleep_for(std::chrono::milliseconds(s));
 }
+
+// Periodically call fn until it returns true or timeout occurs
+template <typename Pred>
+inline void call_until(const std::chrono::milliseconds timeout,
+                       const std::chrono::milliseconds rate, Pred fn) {
+  auto deadline = util::current_time() + timeout;
+  while (fn() && util::current_time() < deadline) {
+    util::sleep_ms(rate);
+  }
+}
+
 }  // namespace util

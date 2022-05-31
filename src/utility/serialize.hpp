aee96942676fb35955c91693d25b86bb722dbd4e
shmocz <112764837+shmocz@users.noreply.github.com>
2022-05-31 14:39:55 +0300

    fixes to serialize utils

--- src/utility/serialize.hpp
+++ src/utility/serialize.hpp
@@ -8,16 +8,18 @@ using u8 = std::uint8_t;
 template <typename T, typename U>
 auto read_obj(U* addr) {
   T t{0};
-  auto b = static_cast<u8*>(&t[0]);
-  std::copy(t, t + sizeof(T), static_cast<u8*>(addr));
+  auto a = reinterpret_cast<const u8*>(addr);
+  auto b = reinterpret_cast<u8*>(&t);
+  std::copy(a, a + sizeof(T), b);
   return t;
 }
 
 template <typename T, typename U>
-auto read_obj_le(U* addr) {
+auto read_obj_le(const U* addr) {
   T t{0};
   auto b = reinterpret_cast<u8*>(&t);
-  std::copy_backward(b, b + sizeof(T), static_cast<u8*>(addr));
+  auto a = static_cast<const u8*>(addr);
+  std::copy_backward(a, a + sizeof(T), b);
   return t;
 }
 

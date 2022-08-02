61948e2b4adf5475d062212a9b75c3c2aab41167
shmocz <112764837+shmocz@users.noreply.github.com>
2022-08-02 07:26:54 +0300

    commands_yr: various improvements
    Rewrite state saving callback using ISCallback interface. Save House
    defeated state and object armor multipliers. Use CBExitGameLoop to delete
    CBSaveState, which then closes record file handle gracefully. Simplify
    storage code. Parse cameos, typeclasses etc. Linter fixes and cleanups.
    Use fmtlib in errors.

--- src/commands_yr.cpp
+++ src/commands_yr.cpp
@@ -4,70 +4,118 @@ using namespace commands_yr;
 using util_command::ISCommand;
 using yrclient::as;
 using yrclient::asptr;
-using yrclient::parse_address;
 using GameT = ra2::game_state::GameState;
+using google::protobuf::RepeatedPtrField;
+using cb_map_t = std::map<std::string, std::unique_ptr<yrclient::ISCallback>>;
+
+// TODO: smarter way to define these
+constexpr char key_callbacks_yr[] = "callbacks_yr";
+constexpr char key_game_state[] = "game_state";
+constexpr char key_raw_game_state[] = "raw_game_state";
+constexpr char key_state_rb[] = "state_rb";
+constexpr char key_exit_gameloop[] = "cb_exit_gameloop";
+constexpr char key_gameloop[] = "cb_gameloop";
+constexpr char key_on_gameloop_exit[] = "on_gameloop_exit";
+constexpr char key_on_frame_update[] = "on_frame_update";
+constexpr char key_save_state[] = "save_state";
+
+struct hook_entry {
+  u32 p_target;
+  size_t code_size;
+};
+
+struct cb_entry {
+  std::string target;
+  std::unique_ptr<yrclient::ISCallback> p_callback;
+};
+
+static std::map<std::string, hook_entry> g_hooks = {
+    {key_on_frame_update, {0xb7e6ac, 8}},
+    {key_on_gameloop_exit, {0xb7e6bc, 6}},
+};
+
+template <typename T>
+T* ensure_storage_value(yrclient::InstrumentationService* I,
+                        yrclient::storage_t* s, const std::string key) {
+  if (s->find(key) == s->end()) {
+    I->store_value(key, new T(), [](void* data) { delete as<T*>(data); });
+  }
+  return as<T*>(s->at(key).get());
+}
 
 static GameT* ensure_raw_gamestate(yrclient::InstrumentationService* I,
                                    yrclient::storage_t* s) {
-  const std::string k = "raw_game_state";
-  if (s->find(k) == s->end()) {
-    I->store_value(k, new GameT(), [](void* data) { delete as<GameT*>(data); });
+  return ensure_storage_value<GameT>(I, s, key_raw_game_state);
+}
+
+static void get_factories(GameT* G,
+                          RepeatedPtrField<yrclient::ra2yr::Factory>* res) {
+  for (auto& v : G->factory_classes()) {
+    auto* f = res->Add();
+    f->set_object_id(reinterpret_cast<u32>(v->object));
+    f->set_owner(reinterpret_cast<u32>(v->owner));
+    f->set_progress_timer(v->production.value);
   }
-  return asptr<GameT>(s->at(k).get());
 }
 
-static void parse_houses(GameT* G) {
-  ra2::state_parser::parse_DVC_HouseClasses(
-      G, reinterpret_cast<void*>(ra2::game_state::p_DVC_HouseClasses));
+static bool is_valid_house(const ra2::objects::HouseClass& H) {
+  return H.start_credits > 0;
 }
 
-static void save_objects(GameT* G) {
-  using namespace ra2::state_parser;
-  using namespace ra2::abstract_types;
-  parse_DVC_Objects(
-      G, reinterpret_cast<void*>(ra2::game_state::p_DVC_TechnoClasses));
+static void get_houses(GameT* G,
+                       RepeatedPtrField<yrclient::ra2yr::House>* res) {
+  for (auto& v : G->house_classes()) {
+    if (!is_valid_house(*v)) {
+      continue;
+    }
+    auto* h = res->Add();
+    h->set_array_index(v->array_index);
+    h->set_current_player(v->current_player);
+    h->set_defeated(v->defeated);
+    h->set_money(v->money);
+    h->set_start_credits(v->start_credits);
+    h->set_name(v->name);
+    h->set_self(v->self);
+    h->set_defeated(v->defeated);
+    h->set_is_game_over(v->is_game_over);
+    h->set_is_loser(v->is_loser);
+    h->set_is_winner(v->is_winner);
+  }
 }
 
-static void parse_building(yrclient::ra2yr::Unit* u,
-                           ra2::objects::BuildingClass* b) {
-  u->set_unit_type("building");
-  u->set_building_state(
-      (yrclient::ra2yr::BuildingState)(((u32)b->build_state_type) + 1u));
-  u->set_queue_building_state(
-      (yrclient::ra2yr::BuildingState)(((u32)b->queue_build_state) + 1u));
-}
-
-static void get_unit(yrclient::ra2yr::Unit* u,
-                     ra2::abstract_types::AbstractTypeClass* atc,
-                     ra2::objects::ObjectClass* v) {
+static void get_object(yrclient::ra2yr::Object* u,
+                       ra2::abstract_types::AbstractTypeClass* atc,
+                       ra2::objects::ObjectClass* v) {
   using ra2::general::AbstractType;
   auto* tc = (ra2::objects::TechnoClass*)(v);
   const auto* ttc = (ra2::type_classes::TechnoTypeClass*)atc;
   if (v->id == 0u) {
     fmt::print("[ERROR] object has NULL id\n");
   }
-  u->set_id(v->id);
+  u->set_pointer_technotypeclass(ttc->pointer_self);
   u->set_health(v->health);
-  u->set_name(atc->name);
-  u->set_cost(ttc->cost);
-  u->set_soylent(ttc->soylent);
-  u->set_owner_id(reinterpret_cast<u32>(tc->owner));
-  u->set_armor((yrclient::ra2yr::Armor)ttc->armor);
-  u->set_speed(ttc->speed);
-  u->set_speed_type((yrclient::ra2yr::SpeedType)ttc->speed_type);
-  u->set_points(ttc->points);
+  u->set_pointer_house(reinterpret_cast<u32>(tc->owner));
+
+  if (yrclient::band<i32>(v->flags, ra2::general::AbstractFlags::Techno)) {
+    auto* t = (ra2::objects::TechnoClass*)v;
+    u->set_armor_multiplier(t->armor_multiplier);
+  }
 
   auto at =
       ra2::utility::get_AbstractType(reinterpret_cast<void*>(atc->p_vtable));
+  // TODO: static LUT
   switch (at.t) {
     case AbstractType::BuildingType:
-      parse_building(u, (ra2::objects::BuildingClass*)v);
+      u->set_object_type(yrclient::ra2yr::ABSTRACT_TYPE_BUILDING);
       break;
     case AbstractType::InfantryType:
-      u->set_unit_type("infantry");
+      u->set_object_type(yrclient::ra2yr::ABSTRACT_TYPE_INFANTRY);
       break;
     case AbstractType::UnitType:
-      u->set_unit_type("vehicle");
+      u->set_object_type(yrclient::ra2yr::ABSTRACT_TYPE_VEHICLE);
+      break;
+    case AbstractType::AircraftType:
+      u->set_object_type(yrclient::ra2yr::ABSTRACT_TYPE_AIRCRAFT);
       break;
     default:
       fmt::print("NO MATCH {}\n", at.name);
@@ -85,33 +133,28 @@ static void get_unit(yrclient::ra2yr::Unit* u,
   coords->set_z(tc->coords.z);
 }
 
-using google::protobuf::RepeatedPtrField;
-
-static void get_factories(GameT* G,
-                          RepeatedPtrField<yrclient::ra2yr::Factory>* res) {
-  for (auto& v : G->factory_classes()) {
-    auto* f = res->Add();
-    f->set_object_id(reinterpret_cast<u32>(v->object));
-    f->set_owner(reinterpret_cast<u32>(v->owner));
-    f->set_progress_timer(v->production.value);
+static void get_object_type_class(yrclient::ra2yr::ObjectTypeClass* t,
+                                  ra2::objects::AbstractTypeClass* v) {
+  t->set_name(v->name);
+  if (ra2::utility::is_technotypeclass(reinterpret_cast<void*>(v->p_vtable))) {
+    auto* ttc = (ra2::type_classes::TechnoTypeClass*)v;
+    t->set_pointer_self(ttc->pointer_self);
+    t->set_cost(ttc->cost);
+    t->set_soylent(ttc->soylent);
+    t->set_armor_type((yrclient::ra2yr::Armor)ttc->armor);
   }
 }
 
-static void get_houses(GameT* G,
-                       RepeatedPtrField<yrclient::ra2yr::House>* res) {
-  for (auto& v : G->house_classes()) {
-    auto* h = res->Add();
-    h->set_array_index(v->array_index);
-    h->set_current_player(v->current_player);
-    h->set_defeated(v->defeated);
-    h->set_money(v->money);
-    h->set_start_credits(v->start_credits);
-    h->set_name(v->name);
-    h->set_self(v->self);
+static void get_object_type_classes(
+    GameT* G, RepeatedPtrField<yrclient::ra2yr::ObjectTypeClass>* r) {
+  for (const auto& [k, v] : G->abstract_type_classes()) {
+    auto* t = r->Add();
+    get_object_type_class(t, v.get());
   }
 }
 
-static void get_units(GameT* G, RepeatedPtrField<yrclient::ra2yr::Unit>* res) {
+static void get_objects(GameT* G,
+                        RepeatedPtrField<yrclient::ra2yr::Object>* res) {
   for (const auto& [k, v] : G->objects) {
     auto* tc = (ra2::objects::TechnoClass*)(v.get());
     try {
@@ -119,100 +162,172 @@ static void get_units(GameT* G, RepeatedPtrField<yrclient::ra2yr::Unit>* res) {
                       .at(reinterpret_cast<u32>(tc->p_type))
                       .get();
       auto* u = res->Add();
-      get_unit(u, atc, v.get());
+      get_object(u, atc, v.get());
     } catch (const std::exception& e) {
-      fmt::print(stderr, "[ERROR] atc fail {},{}\n", static_cast<void*>(tc),
-                 tc->p_type->p_vtable);
+      fmt::print(stderr, "[ERROR] atc fail tc={}, p_type_class={}, what={}\n",
+                 static_cast<void*>(tc), static_cast<void*>(tc->p_type),
+                 e.what());
       continue;
     }
   }
 }
 
-static void parse_factoryclasses(GameT* G) {
-  ra2::state_parser::parse_DVC_FactoryClasses(
-      G, reinterpret_cast<void*>(ra2::game_state::p_DVC_FactoryClasses));
+static void raw_state_to_protobuf(GameT* raw_state,
+                                  yrclient::ra2yr::GameState* state,
+                                  const bool type_classes = false) {
+  state->set_current_frame(
+      serialize::read_obj_le<u32>(as<u32*>(ra2::game_state::current_frame)));
+
+  if (type_classes) {
+    get_object_type_classes(raw_state, state->mutable_object_types());
+  }
+  get_factories(raw_state, state->mutable_factories());
+  get_houses(raw_state, state->mutable_houses());
+  get_objects(raw_state, state->mutable_objects());
 }
 
-void cb_save_state(hook::Hook* h, void* data, X86Regs* state) {
-  using namespace std::literals::chrono_literals;
-  (void)h;
-  (void)state;
-#ifndef NDEBUG
-  static auto cur_time = util::current_time();
-#endif
-  auto I = static_cast<yrclient::InstrumentationService*>(data);
-  auto [mut, s] = I->aq_storage();
-  const std::string k = "game_state";
-  if (s->find(k) == s->end()) {
-    I->store_value(k, new yrclient::ra2yr::GameState(), [](void* data) {
-      delete as<yrclient::ra2yr::GameState*>(data);
-    });
-  }
-  auto* r_game_state = ensure_raw_gamestate(I, s);
-  // TODO: improve this
-  // Save frame directly as protobuf message
-  auto game_state = asptr<yrclient::ra2yr::GameState>(I->get_value(k, false));
-  game_state->set_current_frame(
-      serialize::read_obj_le<u32>(as<u32*>(0xA8ED84)));
+struct CBExitGameLoop : public yrclient::ISCallback {
+  std::string name() override { return key_exit_gameloop; }
+  std::string target() override { return key_on_gameloop_exit; }
+  CBExitGameLoop() {}
+  ~CBExitGameLoop() override {}
 
-#ifndef NDEBUG
-  if ((game_state->current_frame() % 60) == 0) {
-    auto diff = util::current_time() - cur_time;
-    fmt::print(stderr, "frame={},diff={},num units={}\n",
-               game_state->current_frame(), diff, r_game_state->objects.size());
-    cur_time = util::current_time();
-  }
-#endif
+  void do_call(yrclient::InstrumentationService* I) override {
+    // Delete all callbacks except ourselves
+    // NB. the corresponding HookCallback must be removed from Hook object
+    // (shared_ptr would be handy here)
 
-  // Save raw objects
-  parse_houses(r_game_state);
-  save_objects(r_game_state);
-  parse_factoryclasses(r_game_state);
+    auto [lk, hhooks] = I->aq_hooks();
+    auto* callbacks = asptr<cb_map_t>(I->get_value(key_callbacks_yr, true));
+    // Loop through all callbacks
+    std::vector<std::string> keys;
+    std::transform(callbacks->begin(), callbacks->end(),
+                   std::back_inserter(keys), [](auto& v) { return v.first; });
+    for (const auto& k : keys) {
+      if (k != name()) {
+        // Get corresponding hook
+        auto h = std::find_if(hhooks->begin(), hhooks->end(), [&](auto& a) {
+          return (a.second.name() == callbacks->at(k)->target());
+        });
+        // Remove callback's reference from Hook
+        if (h == hhooks->end()) {
+          throw std::runtime_error("hook not found");
+        }
+        h->second.remove_callback(k);
+        // Delete callback object
+        callbacks->erase(k);
+      }
+    }
+  }
+};
+
+struct CBSaveState : public yrclient::ISCallback {
+  const std::string record_path;
+  yrclient::CompressedOutputStream* out;
+  explicit CBSaveState(const std::string record_path)
+      : record_path(record_path),
+        out(new yrclient::CompressedOutputStream(record_path)) {}
+  ~CBSaveState() override { delete out; }
+  std::string name() override { return key_save_state; }
+  std::string target() override { return key_on_frame_update; }
+  void do_call(yrclient::InstrumentationService* I) override {
+    using namespace std::literals::chrono_literals;
+
+    auto [mut, s] = I->aq_storage();
+    (void)ensure_storage_value<yrclient::ra2yr::GameState>(I, s,
+                                                           key_game_state);
+    auto* r_game_state = ensure_raw_gamestate(I, s);
+
+    // Parse type classes only once
+    bool has_typeclasses = r_game_state->abstract_type_classes().empty();
+    if (has_typeclasses) {
+      ra2::state_parser::parse_AbstractTypeClasses(
+          r_game_state,
+          reinterpret_cast<void*>(ra2::game_state::p_DVC_AbstractTypeClasses));
+      ra2::state_parser::parse_cameos(r_game_state);
+    }
+
+    // Save raw objects
+    ra2::state_parser::parse_DVC_HouseClasses(
+        r_game_state,
+        reinterpret_cast<void*>(ra2::game_state::p_DVC_HouseClasses));
+    ra2::state_parser::parse_DVC_Objects(
+        r_game_state,
+        reinterpret_cast<void*>(ra2::game_state::p_DVC_TechnoClasses));
+    ra2::state_parser::parse_DVC_FactoryClasses(
+        r_game_state,
+        reinterpret_cast<void*>(ra2::game_state::p_DVC_FactoryClasses));
+
+    yrclient::ra2yr::GameState gbuf;
+
+    raw_state_to_protobuf(r_game_state, &gbuf, has_typeclasses);
+
+    if (out != nullptr) {
+      google::protobuf::io::CodedOutputStream co(&out->s_g);
+
+      if (!yrclient::write_message(&gbuf, &co)) {
+        throw std::runtime_error("write_message");
+      }
+    }
+  }
+};
+
+// TODO: customizable output path
+static void init_callbacks(yrclient::InstrumentationService* I) {
+  I->store_value(key_callbacks_yr, new cb_map_t(),
+                 [](void* data) { delete as<cb_map_t*>(data); });
+
+  auto t = std::to_string(static_cast<std::uint64_t>(
+      std::chrono::high_resolution_clock::now().time_since_epoch().count()));
+
+  std::string record_out = yrclient::join_string({"record", t, "pb.gz"}, ".");
+  fmt::print(stderr, "save to {}\n", record_out);
+  auto* v = asptr<cb_map_t>(I->get_value(key_callbacks_yr, false));
+  std::vector<yrclient::ISCallback*> cbs{new CBSaveState(record_out),
+                                         new CBExitGameLoop()};
+  for (auto* cb : cbs) {
+    v->try_emplace(cb->name(), cb);
+  }
 }
-
-struct hook_entry {
-  u32 p_target;
-  size_t code_size;
-};
-
-struct cb_entry {
-  std::string target;
-  hook::Hook::hook_cb_t p_callback;
-};
-
-static std::map<std::string, hook_entry> hooks = {
-    {"on_frame_update", {0xb7e6ac, 8}}};
-
-static std::map<std::string, cb_entry> callbacks = {
-    {"save_state", {"on_frame_update", &cb_save_state}}};
 
 static std::map<std::string, command::Command::handler_t> commands = {
     {"CreateHooks",
      [](command::Command* c) {
        ISCommand<yrclient::commands::CreateHooks> Q(c);
-       for (auto& [k, v] : hooks) {
+       for (auto& [k, v] : g_hooks) {
          Q.I()->create_hook(k, reinterpret_cast<u8*>(v.p_target), v.code_size);
        }
      }},
     {"CreateCallbacks",
      [](command::Command* c) {
        ISCommand<yrclient::commands::CreateCallbacks> Q(c);
+       auto [lk_s, s] = Q.I()->aq_storage();
+       if (s->find(key_callbacks_yr) == s->end()) {
+         init_callbacks(Q.I());
+       }
+       auto g_callbacks = asptr<cb_map_t>(s->at(key_callbacks_yr).get());
+       lk_s.unlock();
        auto [lk, hhooks] = Q.I()->aq_hooks();
-       for (auto& [k, v] : callbacks) {
-         auto target = v.target;
+       for (auto& [k, v] : *g_callbacks) {
+         auto target = v->target();
+         auto cb = v.get();
          auto h = std::find_if(hhooks->begin(), hhooks->end(), [&](auto& a) {
            return (a.second.name() == target);
          });
          if (h == hhooks->end()) {
-           throw yrclient::general_error(std::string("No such hook: ") +
-                                         target);
+           throw yrclient::general_error(
+               fmt::format("No such hook {}", target));
          }
          if (h->second.callbacks().size() > 0) {
            throw yrclient::general_error(
-               std::string("hook already has a callback"));
+               fmt::format("Hook {} already has a callback", target));
          }
 
-         h->second.add_callback(hook::Hook::HookCallback{v.p_callback, Q.I()});
+         h->second.add_callback(hook::Hook::HookCallback{
+             [cb](hook::Hook* h, void* user_data, X86Regs* state) {
+               cb->call(h, user_data, state);
+             },
+             Q.I(), 0u, 0u, k});
        }
      }},
     {"GetGameState",
@@ -221,51 +336,50 @@ static std::map<std::string, command::Command::handler_t> commands = {
        // Copy saved game state
        auto [mut, s] = Q.I()->aq_storage();
        auto res = Q.command_data().mutable_result();
-       auto* val = asptr<yrclient::ra2yr::GameState>(s->at("game_state").get());
        auto* state = res->mutable_state();
        // state->CopyFrom(*val);
        auto* G = ensure_raw_gamestate(Q.I(), s);
-       get_units(G, state->mutable_units());
-       get_factories(G, state->mutable_factories());
-       get_houses(G, state->mutable_houses());
+       raw_state_to_protobuf(G, state);
      }},
     {"GetTypeClasses",
      [](command::Command* c) {
-       using namespace ra2::state_parser;
-       using namespace ra2::abstract_types;
        ISCommand<yrclient::commands::GetTypeClasses> Q(c);
        auto [mut, s] = Q.I()->aq_storage();
        auto G = ensure_raw_gamestate(Q.I(), s);
        auto res = Q.command_data().mutable_result();
-       parse_AbstractTypeClasses(
-           G,
-           reinterpret_cast<void*>(ra2::game_state::p_DVC_AbstractTypeClasses));
        for (auto& [k, v] : G->abstract_type_classes()) {
          auto* tc = res->add_classes();
          tc->set_name(v->name);
+         if (ra2::utility::is_technotypeclass(
+                 reinterpret_cast<void*>(v->p_vtable))) {
+           auto* ttc = (ra2::type_classes::TechnoTypeClass*)v.get();
+           if (ttc->p_cameo != nullptr) {
+             auto& cameo = G->cameos.at(reinterpret_cast<u32*>(ttc->p_cameo));
+             auto* f = tc->mutable_cameo()->add_frames();
+             auto& pd = cameo->pixel_data[0];
+             f->assign(pd.begin(), pd.end());
+           }
+         }
        }
      }},
     {"GetObjects",
      [](command::Command* c) {
-       using namespace ra2::state_parser;
-       using namespace ra2::abstract_types;
        ISCommand<yrclient::commands::GetObjects> Q(c);
        auto [mut, s] = Q.I()->aq_storage();
        auto G = ensure_raw_gamestate(Q.I(), s);
        auto res = Q.command_data().mutable_result();
-       get_units(G, res->mutable_units());
+       yrclient::ra2yr::GameState state;
+       raw_state_to_protobuf(G, &state);
+       res->mutable_units()->CopyFrom(state.units());
      }},
     {"GetHouses", [](command::Command* c) {
-       using namespace ra2::state_parser;
-       using namespace ra2::abstract_types;
        ISCommand<yrclient::commands::GetHouses> Q(c);
        auto [mut, s] = Q.I()->aq_storage();
        auto G = ensure_raw_gamestate(Q.I(), s);
        auto res = Q.command_data().mutable_result();
-       parse_DVC_HouseClasses(
+       ra2::state_parser::parse_DVC_HouseClasses(
            G, reinterpret_cast<void*>(ra2::game_state::p_DVC_HouseClasses));
        // WIP: typeclass and superweapons
-       DPRINTF("num classes=%d\n", G->house_classes().size());
        get_houses(G, res->mutable_houses());
      }}};
 
--- src/commands_yr.hpp
+++ src/commands_yr.hpp
@@ -2,9 +2,10 @@
 #include "protocol/protocol.hpp"
 
 #include "command/command.hpp"
-#include "command/command_manager.hpp"
 #include "errors.hpp"
+#include "google/protobuf/io/coded_stream.h"
 #include "google/protobuf/repeated_ptr_field.h"
+#include "hook.hpp"
 #include "instrumentation_service.hpp"
 #include "ra2/abstract_types.hpp"
 #include "ra2/game_state.hpp"
@@ -12,17 +13,16 @@
 #include "ra2/objects.hpp"
 #include "ra2/state_parser.hpp"
 #include "ra2/type_classes.hpp"
+#include "ra2/utility.hpp"
 #include "util_command.hpp"
 #include "util_string.hpp"
 #include "utility.h"
-#include "utility/serialize.hpp"
-#include "utility/time.hpp"
 
-#include <fmt/chrono.h>
-
+#include <algorithm>
 #include <map>
 #include <memory>
 #include <string>
+#include <vector>
 
 namespace commands_yr {
 

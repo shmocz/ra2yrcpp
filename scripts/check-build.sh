c3bda2eb694a3dd5f9f389bc6f2c84375f696ed5
shmocz <112764837+shmocz@users.noreply.github.com>
2023-09-17 15:56:05 +0300

    fix: Ignore xbyak warnings in build log

--- scripts/check-build.sh
+++ scripts/check-build.sh
@@ -2,8 +2,8 @@
 
 [ ! -f "$1" ] && exit 1
 
-# FIXME: avoid creating temporary file
-grep -P "warning" "$1" | grep -Pv '(Wunknown-pragmas|Wcomment|#pragma\s+warning|proto)' >err.log
+# TODO(shmocz): avoid creating temporary file
+grep -P "warning" "$1" | grep -Pv '(Wunknown-pragmas|Wcomment|#pragma\s+warning|proto|xbyak\.h)' >err.log
 
 e="$(cat err.log)"
 
